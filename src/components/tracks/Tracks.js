5ae05ee4073583dee3ff273eb18e77de8820db92
RaafinHaswoto <raafi.haswoto@gmail.com>
2018-11-01 23:40:36 +0700

    Create Tracks

---
+++ src/components/tracks/Tracks.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Consumer } from '../../context';
+
+export default class Tracks extends Component {
+  render() {
+    return (
+      <Consumer>
+          {value => {
+              console.log(value)
+              return <h1>tracks</h1>
+          }}
+      </Consumer>
+    )
+  }
+}
+ 
